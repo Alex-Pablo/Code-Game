13164ff9e2b87b6adb792d9edbb30428e0e9fb83
Alex-Pablo <alexpabloanibal@gmail.com>
2023-12-09 21:43:19 -0600

    initial commit

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -1,3 +1,26 @@
 import { Routes } from '@angular/router';
+import { LayoutComponent } from './components/layout/layout.component';
+import { HomeComponent } from './views/home/home.component';
+import { GameComponent } from './views/game/game.component';
 
-export const routes: Routes = [];
+export const routes: Routes = [
+    {
+        path: '',
+        redirectTo: 'game',
+        pathMatch:'full'
+    },
+    {
+        path:'',
+        component: LayoutComponent,
+        children:[
+            {
+                path:'home',
+                component:HomeComponent
+            },
+            {
+                path:'game',
+                component:GameComponent
+            }
+        ]
+    }
+];
